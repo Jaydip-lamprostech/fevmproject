c30f99ff02544e00b55368923bd749499360cd1f
Jaydip-lamprostech <jaydip.lamprostech@gmail.com>
2022-11-20 15:45:37 +0530

    changes in the links

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,143 @@ export default function Home() {
   }, []);
   const logoref = useRef();
 
-  const lending = `smart contract`;
+  const lending = `<div className={styles.main}>
+  <section className={styles.hero}>
+    <div className={styles.heroinsidefirst}>
+      <h1 className="heroh1 animate__animated animate__fadeInDown">
+        Smart Contracts
+      </h1>
+      <p className="herop1 animate__animated animate__fadeInDown">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+        reiciendis nam vero vitae rerum expedita sequi totam commodi
+        dignissimos numquam?
+      </p>
+      <button className="herofirstbutton animate__animated animate__fadeInDown">
+        Learn more
+      </button>
+    </div>
+    <div className="heroinsidesecond animate__animated animate__fadeIn">
+      <div className="inside animate__animated animate__fadeIn">
+        <Image className="heroimage" src={boy} alt="boy-image" />
+      </div>
+      <BackgroundAnimation />
+    </div>
+  </section>
+  <section className={styles.secondsection}>
+    <h2 className="contractsheading">Contracts</h2>
+    <div className={styles.contractdiv}>
+      <h2>Lending contract</h2>
+      <span className={styles.features}>
+        <ul>
+          <li>
+            Create a pool contract that accepts deposit from lenders and
+            borrow money to the borrowers
+          </li>
+          <li>
+            Lenders can lend any amount of money and earn some interest for
+            it.
+          </li>
+          <li>
+            User or borrower can borrow some amount of tokens (limited) ,
+            and pay back with interest for some time period.
+          </li>
+          <li>
+            Interest is calculated according the interest rate and borrowing
+            time peroid
+          </li>
+          <li>
+            Lender can withdraw the amount later with extra interest earning
+          </li>
+          <li>
+            Other functions can be called to determine the balance at any
+            point of time , and the rewards earned
+          </li>
+        </ul>
+      </span>
+      <div className="parentofcode">
+        <span className={styles.code}>
+          <pre className="line-numbers">
+            <code className="language-jsx">{lending}</code>
+          </pre>
+        </span>
+      </div>
+    </div>
+    <div className={styles.contractdiv}>
+      <h2>Lending contract</h2>
+      <span className={styles.features}>
+        <ul>
+          <li>
+            Create a pool contract that accepts deposit from lenders and
+            borrow money to the borrowers
+          </li>
+          <li>
+            Lenders can lend any amount of money and earn some interest for
+            it.
+          </li>
+          <li>
+            User or borrower can borrow some amount of tokens (limited) ,
+            and pay back with interest for some time period.
+          </li>
+          <li>
+            Interest is calculated according the interest rate and borrowing
+            time peroid
+          </li>
+          <li>
+            Lender can withdraw the amount later with extra interest earning
+          </li>
+          <li>
+            Other functions can be called to determine the balance at any
+            point of time , and the rewards earned
+          </li>
+        </ul>
+      </span>
+      <div className="parentofcode">
+        <span className={styles.code}>
+          <pre className="line-numbers">
+            <code className="language-jsx">{lending}</code>
+          </pre>
+        </span>
+      </div>
+    </div>
+    <div className={styles.contractdiv}>
+      <h2>Lending contract</h2>
+      <span className={styles.features}>
+        <ul>
+          <li>
+            Create a pool contract that accepts deposit from lenders and
+            borrow money to the borrowers
+          </li>
+          <li>
+            Lenders can lend any amount of money and earn some interest for
+            it.
+          </li>
+          <li>
+            User or borrower can borrow some amount of tokens (limited) ,
+            and pay back with interest for some time period.
+          </li>
+          <li>
+            Interest is calculated according the interest rate and borrowing
+            time peroid
+          </li>
+          <li>
+            Lender can withdraw the amount later with extra interest earning
+          </li>
+          <li>
+            Other functions can be called to determine the balance at any
+            point of time , and the rewards earned
+          </li>
+        </ul>
+      </span>
+      <div className="parentofcode">
+        <span className={styles.code}>
+          <pre className="line-numbers">
+            <code className="language-jsx">{lending}</code>
+          </pre>
+        </span>
+      </div>
+    </div>
+  </section>
+</div>`;
   // const button = document.getElementById("logo_image");
 
   return (
@@ -46,7 +182,7 @@ export default function Home() {
       </section>
       <section className={styles.secondsection}>
         <h2 className="contractsheading">Contracts</h2>
-        <div className={styles.contractdiv}>
+        <div className={styles.contractdiv} id="lending">
           <h2>Lending contract</h2>
           <span className={styles.features}>
             <ul>
@@ -75,7 +211,7 @@ export default function Home() {
               </li>
             </ul>
           </span>
-          <div className={styles.parentofcode}>
+          <div className="parentofcode">
             <span className={styles.code}>
               <pre className="line-numbers">
                 <code className="language-jsx">{lending}</code>
@@ -83,7 +219,7 @@ export default function Home() {
             </span>
           </div>
         </div>
-        <div className={styles.contractdiv}>
+        <div className={styles.contractdiv} id="staking">
           <h2>Lending contract</h2>
           <span className={styles.features}>
             <ul>
@@ -112,7 +248,7 @@ export default function Home() {
               </li>
             </ul>
           </span>
-          <div className={styles.parentofcode}>
+          <div className="parentofcode">
             <span className={styles.code}>
               <pre className="line-numbers">
                 <code className="language-jsx">{lending}</code>
@@ -120,7 +256,7 @@ export default function Home() {
             </span>
           </div>
         </div>
-        <div className={styles.contractdiv}>
+        <div className={styles.contractdiv} id="vault">
           <h2>Lending contract</h2>
           <span className={styles.features}>
             <ul>
@@ -149,7 +285,7 @@ export default function Home() {
               </li>
             </ul>
           </span>
-          <div className={styles.parentofcode}>
+          <div className="parentofcode">
             <span className={styles.code}>
               <pre className="line-numbers">
                 <code className="language-jsx">{lending}</code>
